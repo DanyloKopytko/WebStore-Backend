c9e40443a03434c89d7d214c76fec69303d3b9b9
Roman Yatsynych <rom.yatsynych@gmail.com>
2020-09-09 14:46:20 +0300

    filter for goods + excelParser fix

--- controllers/goods.controllers/getByCategory.controller.js
+++ controllers/goods.controllers/getByCategory.controller.js
@@ -1,13 +1,31 @@
+const { Op } = require('sequelize');
+
 const db = require('../../database').getInstance();
 
 module.exports = async (req, res) => {
     try {
-        const { category_id } = req.params;
+        const {category_id} = req.params;
 
         const GoodsModel = db.getModel('Goods');
+        const CategoriesModel = db.getModel('Categories');
 
-        const goods = await GoodsModel.findAll({ where: {category_id} });
+        const filter = {
+            category_id
+        };
 
+        filter.naming = {
+            [Op.startsWith]: req.query.naming
+        };
+
+        filter.price = {
+            [Op.between] : [req.query.priceMin, req.query.priceMax]
+        };
+
+        const goods = await GoodsModel.findAll({
+            where: filter,
+            include: [{model: CategoriesModel, as: 'Category'}]
+        });
+            
         return res.status(200).send({error: false, goods});
     } catch (e) {
         console.log(e);
